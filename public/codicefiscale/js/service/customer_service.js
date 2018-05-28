b31ac2147273d1f36beaaafef4b8538672f2213c
lucabindi <luca.bindi@oracle.com>
2018-05-28 23:52:11 +0200

    Add files via upload

---
+++ public/codicefiscale/js/service/customer_service.js
@@ -0,0 +1,49 @@
+'use strict';
+
+angular.module('myApp').factory('CodiceFiscaleProvider', ['$http', '$q', function($http, $q){
+
+  const CF_REST_SERVICE_URI = './calcolaCodiceFiscale';
+//  const CM_REST_SERVICE_URI = './getComuni?nome=';
+  const CM_REST_SERVICE_URI = 'http://192.168.99.110:8080/ords/ORCL/movie/codicefiscale/comuni/?nome=';
+
+  var factory = {
+    callCodiceFiscale: restCodiceFiscale,
+    getElencoComuni: restElencoComuni
+  };
+
+  return factory;
+
+  function restCodiceFiscale(user) {
+    var deferred = $q.defer();
+
+    $http.post(CF_REST_SERVICE_URI, JSON.stringify(user))
+        .then(
+        function (response) {
+            deferred.resolve(response.data);
+        },
+        function(errResponse){
+            console.error('Errore mentre calcolo il codice fiscale', errResponse);
+            deferred.reject(errResponse);
+        }
+    )
+    return deferred.promise;
+  }
+
+
+  function restElencoComuni(partialName) {
+    var deferred = $q.defer();
+        console.log('luca1=', partialName)
+        $http.get(CM_REST_SERVICE_URI + partialName)
+        .then(
+          function (response) {
+            deferred.resolve(response.data);
+          },
+          function(errResponse){
+            console.error('Errore mentre carico lista parziale comuni ', errResponse);
+            deferred.reject(errResponse);
+        }
+    );
+    return deferred.promise;
+  }
+
+}]);
